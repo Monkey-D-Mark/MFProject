55937a4b928967d3f90a7a27d08cb45ecacef848
332738926Mark <shendan@shendandeMac-mini.local>
2017-03-16 15:17:23 +0800

    Initial Commit

---
+++ MKCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MKCode
+//
+//  Created by shendan on 2017/3/16.
+//  Copyright © 2017年 Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
